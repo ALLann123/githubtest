5e626189f239ac4f788adf75dae2b5a88d780c92
root <root@kali.kali>
2024-10-08 12:27:27 +0200

    quize on me

---
+++ madlib.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+adj=input("Is programming Easy or hard?")
+lang=input("Which is your favorite Language?")
+soft_one=input("Name something you developed?")
+soft_last=input("Name the last thing you coded?")
+
+madlib=f"Computer programming is {adj}, I enjoy writting my code in {lang}. I have developed {soft_one} and offlate also {soft_last}" 
+print()
+print("[+]",madlib)
